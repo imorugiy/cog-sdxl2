4c047a40d35605916186871cb9244a41818a2320
Ivan Moruhyi <imorugiy@gmail.com>
2024-03-28 13:13:40 -0400

    sdxl variant

--- predict.py
+++ predict.py
@@ -1,36 +1,36 @@
 import os
 import shutil
+import sys
+
+sys.path.extend(["/IP-Adapter"])
+
 from typing import List
+from PIL import Image
 from cog import BasePredictor, Path, Input
 from diffusers.utils import load_image
 from diffusers import DDIMScheduler, StableDiffusionPipeline
+from ip_adapter import IPAdapterPlusXL
+from ip_adapter.custom_pipelines import StableDiffusionXLCustomPipeline
 import torch
 
-MODEL_NAME = "SG161222/Realistic_Vision_V3.0_VAE"
-MODEL_CACHE = "cache"
+MODEL_NAME = "stabilityai/stable-diffusion-xl-base-1.0"
+MODEL_CACHE = "sdxl-cache"
+
+image_encoder_path = "/IP-Adapter/models/image_encoder/"
+ip_ckpt = "/IP-Adapter/sdxl_models/ip-adapter-plus-face_sdxl_vit-h.bin"
+device = "cuda"
 
 FACE_PROMPT = "RAW photo, close up portrait of an African woman, head shot, looking at camera, aged 28, curvy, brown eyes, lips, smile, long hair"
 
 
 class Predictor(BasePredictor):
     def setup(self):
-        noise_scheduler = DDIMScheduler(
-            num_train_timesteps=1000,
-            beta_start=0.00085,
-            beta_end=0.012,
-            beta_schedule="scaled_linear",
-            clip_sample=False,
-            set_alpha_to_one=False,
-            steps_offset=1,
-        )
-
-        self.pipe = StableDiffusionPipeline.from_pretrained(
+        self.pipe = StableDiffusionXLCustomPipeline.from_pretrained(
             MODEL_NAME,
-            scheduler=noise_scheduler,
+            torch_dtype=torch.float16,
+            add_watermarker=False,
             cache_dir=MODEL_CACHE,
         )
-
-        self.pipe.to("cuda")
 
     def load_image(self, path):
         shutil.copyfile(path, "/tmp/image.png")
@@ -54,57 +54,43 @@ class Predictor(BasePredictor):
         num_inference_steps: int = Input(
             description="num_inference_steps", ge=0, le=100, default=30
         ),
-        width: int = Input(description="Width", ge=0, le=1920, default=512),
-        height: int = Input(description="Height", ge=0, le=1920, default=768),
+        width: int = Input(description="Width", ge=0, le=1920, default=1024),
+        height: int = Input(description="Height", ge=0, le=1920, default=1536),
         seed: int = Input(
             description="Seed (0 = random, maximum: 2147483647)", default=0
+        ),
+        scale: float = Input(
+            description="Scale (influence of input image on generation)",
+            ge=0.0,
+            le=1.0,
+            default=0.6,
         ),
     ) -> List[Path]:
         if seed == 0:
             seed = int.from_bytes(os.urandom(2), byteorder="big")
-        generator = torch.Generator("cuda").manual_seed(seed)
 
-        output_paths = []
+        image = Image.open(face_image)
+        image.resize((512, 512))
 
-        if face_image:
-            print("face image provided")
-            img0 = self.load_image(face_image)
-        else:
-            print("face image not provided, using face prompt to generate head photo")
-            output = self.pipe(
-                prompt=face_prompt,
-                negative_prompt=negative_prompt,
-                generator=generator,
-                width=512,
-                height=512,
-                num_inference_steps=num_inference_steps,
-            ).images
-
-            for i, image in enumerate(output):
-                output_path = f"/tmp/out-{i}.png"
-                image.save(output_path)
-                output_paths.append(Path(output_path))
-
-            img0 = self.load_image("/tmp/out-0.png")
-
-        self.pipe.load_ip_adapter(
-            "h94/IP-Adapter",
-            subfolder="models",
-            weight_name="ip-adapter-plus-face_sd15.bin",
+        ip_model = IPAdapterPlusXL(
+            self.pipe, image_encoder_path, ip_ckpt, device, num_tokens=16
         )
 
-        output = self.pipe(
+        images = ip_model.generate(
+            pil_image=image,
+            num_samples=1,
+            seed=seed,
             prompt=prompt,
             negative_prompt=negative_prompt,
-            ip_adapter_image=img0,
             width=width,
             height=height,
             num_inference_steps=num_inference_steps,
-            generator=generator,
-        ).images
+            scale=scale,
+        )
 
-        for i, image in enumerate(output):
-            output_path = f"/tmp/out-{i+1}.png"
+        output_paths = []
+        for i, image in enumerate(images):
+            output_path = f"/tmp/out-{i}.png"
             image.save(output_path)
             output_paths.append(Path(output_path))
 
